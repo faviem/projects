b5509004df22894b955a3e3f7e4d330d9a17fbd9
faviem <fvemile2012@gmail.com>
2016-05-06 13:32:36 +0100

    Updated production config

--- src/BZ/ControllerBundle/Controller/DefaultController.php
+++ src/BZ/ControllerBundle/Controller/DefaultController.php
@@ -11,12 +11,67 @@ use BZ\ModelBundle\Form\RequetePersonneMoraleType;
 use BZ\ModelBundle\Form\RequetePersonnePhysiqueType;
 use BZ\ModelBundle\Entity\ReponseTraitementRequete;
 use BZ\ModelBundle\Form\ReponseTraitementRequeteType;
+
+use BZ\ModelBundle\Entity\Agent;
+use BZ\ModelBundle\Entity\Profil;
+use BZ\UserBundle\Entity\User;
+use BZ\ModelBundle\Entity\ModeSaisine;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+         $em = $this->getDoctrine()->getManager();
+         for($i=5; $i>0; $i--)
+        {
+            $modesaisine= new ModeSaisine;
+            switch ($i)
+            {
+                case 5: 
+                    $modesaisine->setLibelle('Appel téléphonique');
+                    break;
+                case 4: 
+                    $modesaisine->setLibelle('Courier');
+                    break;
+                case 3: 
+                    $modesaisine->setLibelle('Accueil du siège');
+                    break;
+                case 2: 
+                    $modesaisine->setLibelle('Plate-forme *SysGRU*');
+                    break;
+                case 1: 
+                    $modesaisine->setLibelle('Autres');
+                    break;
+            }
+            $em->persist($modesaisine);
+        }
+        $em->flush();
+        $profil = new Profil;
+        $profil->setCode("ROLE_ADMIN");
+        $profil->setLibelle("Administrateur");
+        $em->persist($profil);
+        $em->flush();
+        $user= new User;
+        $user->setUsername('admin');        
+        $user->setEnabled(true);
+        $user->setEmail('faviem2012@gmail.com');
+        $user->setPlainPassword('admin');
+        $user->setType('DIP');
+        $user->setRoles(array('ROLE_ADMIN'));
+        $user->setProfil($profil);
+        $agent = new Agent();
+        $agent->setNom("FADONOUGBO");
+        $agent->setPrenom("Emile");
+        $agent->setLoginpersist("admin");
+        $agent->setEstdelete(false);
+//        $agent->setService(null);
+        $agent->setUser($user);
+        // On le persiste
+        $em->persist($agent);
+        $em->flush();
+        
+        
         if (!$this->isGranted('ROLE_USER')) {
             
            $modesaisine= $this->getDoctrine()
